a492f58a291e8bdc7f8909b0ca198d229ccc15d8
NiharikaKotha9 <niharikakotha999@gmail.com>
2025-03-18 18:20:12 +0530

    Update Basic.java
    Updating

--- Basic.java
+++ Basic.java
@@ -3,9 +3,9 @@ package inheritance;
 public class Basic {
 
 	public static void main(String[] args) {
-		int a=20;
+		int a=60;
 		int b=60;
-		int c=a+b;
+		int c=a-b;
 		System.out.println("The value of c is "+c);
 	}
 
