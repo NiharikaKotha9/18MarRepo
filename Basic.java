671816195dc03ee8ddd69e028a68cea66b584331
NiharikaKotha <niharikakotha999@gmail.com>
2025-03-18 16:24:24 +0530

    Comitting my changes

---
+++ Basic.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("The value of c is "+c);
+	}
+
+}
